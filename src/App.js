dd0872cfee26a07604cbac297aa1aea357babacd
Szymon Mrzyglod <szymon.mr@gmail.com>
2021-10-10 11:15:20 +0200

    change App to function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, {Component, useEffect, useState} from "react";
 import './App.css';
 import Header from './components/Header/Header';
 import Menu from './components/Menu/Menu';
@@ -11,157 +11,87 @@ import ThemeButton from "./components/UI/ThemeButton/ThemeButton";
 import ThemeContext from "./context/themeContext";
 import AuthContext from "./context/authContext";
 
-class App extends Component {
-  hotels = [
-    {
-      id: 1,
-      name: 'Pod akacjami',
-      city: 'Warszawa',
-      rating: 8.3,
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aspernatur repudiandae eius. Beatae error nulla nisi tempora rerum? Voluptatum autem impedit totam, sint ex quos porro quo blanditiis quis! Distinctio?',
-      image: ''
-    },
-    {
-      id: 2,
-      name: 'Dębowy',
-      city: 'Lublin',
-      rating: 7.2,
-      description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aspernatur repudiandae eius. Beatae error nulla nisi tempora rerum? Voluptatum autem impedit totam, sint ex quos porro quo blanditiis quis! Distinctio?',
-      image: ''
-    },
-  ]
-  state={
-    hotels: [],
-    loading: true,
-    theme: 'primary'
-  }
-  handleSearch(inputValue){
-    const hotels = [...this.hotels]
+const beckendHotels = [
+  {
+    id: 1,
+    name: 'Pod akacjami',
+    city: 'Warszawa',
+    rating: 8.3,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aspernatur repudiandae eius. Beatae error nulla nisi tempora rerum? Voluptatum autem impedit totam, sint ex quos porro quo blanditiis quis! Distinctio?',
+    image: ''
+  },
+  {
+    id: 2,
+    name: 'Dębowy',
+    city: 'Lublin',
+    rating: 7.2,
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aspernatur repudiandae eius. Beatae error nulla nisi tempora rerum? Voluptatum autem impedit totam, sint ex quos porro quo blanditiis quis! Distinctio?',
+    image: ''
+  },
+];
+
+
+
+function App() {
+
+  const [hotels, setHotels] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [theme, setTheme] = useState('primary');
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const changeTheme = () => {
+    const newTheme = theme === 'primary' ? 'danger' : 'primary';
+      setTheme(newTheme)
+  };
+
+  const handleSearch = inputValue => {
+    const newHotels = [...beckendHotels]
             .filter(hotel => hotel.name.toLowerCase().includes(inputValue.toLowerCase()))
-    this.setState({
-      hotels
-    })
+    setHotels(newHotels)
   }
 
-  changeTheme = () => {
-    const newTheme = this.state.theme === 'primary' ? 'danger' : 'primary';
-    this.setState({
-      theme: newTheme
-    })
-  }
-
-  componentDidMount(){
+  useEffect(() => {
     setTimeout(()=> {
-      this.setState({
-        hotels: this.hotels,
-        loading: false,
-        isAuthenticated: false,
-      })
+      setHotels(beckendHotels);
+      setLoading(false);
     },1000)
-  }
+  }, []);
 
-  render(){
-    const header =(
-      <Header>
-      <Searchbar onSearch={inputValue => this.handleSearch(inputValue)}/>
-      <ThemeButton />
-      </Header>
-    );
-    const menu = <Menu />;
-    const content = (          
-      this.state.loading ? 
-      (<LoadingIcon />
-      ) :(
-      <Hotels 
-      hotels={this.state.hotels}/>)
-    );
-    const footer = <Footer />
-    return (
-      <AuthContext.Provider value={{
-        isAuthenticated: this.state.isAuthenticated,
-        login: () => this.setState({isAuthenticated: true}),
-        logout: () => this.setState({isAuthenticated: false}),
-      }}>
-        <ThemeContext.Provider value={{
-          color: this.state.theme,
-          changeTheme: this.changeTheme
-          }}>
-          <Layout 
-            header={header}
-            menu={menu}
-            content={content}
-            footer={footer}
-          />
-        </ThemeContext.Provider>
-      </AuthContext.Provider>
-    );
-  }
-}
+  const header =(
+    <Header>
+    <Searchbar onSearch={inputValue => handleSearch(inputValue)}/>
+    <ThemeButton />
+    </Header>
+  );
+  const menu = <Menu />;
+  const content = (          
+    loading ? 
+    (<LoadingIcon />
+    ) :(
+    <Hotels 
+    hotels={hotels}/>)
+  );
+  const footer = <Footer />
+
+  return(
+    <AuthContext.Provider value={{
+      isAuthenticated: isAuthenticated,
+      login: () => setIsAuthenticated(true),
+      logout: () => setIsAuthenticated(false),
+    }}>
+      <ThemeContext.Provider value={{
+        color: theme,
+        changeTheme: changeTheme
+        }}>
+        <Layout 
+          header={header}
+          menu={menu}
+          content={content}
+          footer={footer}
+        />
+      </ThemeContext.Provider>
+    </AuthContext.Provider>
+  )
+};
 
 export default App;
-
-// import {useEffect, useState} from "react";
-// import './App.css';
-// import Header from './components/Header/Header';
-// import Menu from './components/Menu/Menu';
-// import Hotels from './components/Hotels/Hotels';
-// import Searchbar from "./components/UI/Searchbar/Searchbar";
-// import LoadingIcon from "./components/UI/LoadingIcon/LoadingIcon";
-// import Layout from "./components/Layout/Layout";
-// import Footer from "./components/Footer/Footer";
-
-// const backendHotels = [
-//   {
-//     id: 1,
-//     name: 'Pod akacjami',
-//     city: 'Warszawa',
-//     rating: 8.3,
-//     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aspernatur repudiandae eius. Beatae error nulla nisi tempora rerum? Voluptatum autem impedit totam, sint ex quos porro quo blanditiis quis! Distinctio?',
-//     image: ''
-//   },
-//   {
-//     id: 2,
-//     name: 'Dębowy',
-//     city: 'Lublin',
-//     rating: 7.2,
-//     description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aspernatur repudiandae eius. Beatae error nulla nisi tempora rerum? Voluptatum autem impedit totam, sint ex quos porro quo blanditiis quis! Distinctio?',
-//     image: ''
-//   },
-// ];
-
-// function App() {
-//   const [hotels, setHotels] = useState([])
-//   const [loading, setLoading] = useState(true);
-
-//   const handleSearch = inputValue => {
-//     const newHotels = [...backendHotels]
-//             .filter(hotel => hotel.name.toLowerCase().includes(inputValue.toLowerCase()))
-//     setHotels(newHotels);
-//   }
-
-//   useEffect(() =>{   
-//             setTimeout(()=> {
-//               setHotels(backendHotels);
-//               setLoading(false)
-//             },1000)
-//           },[]);
- 
-
-//   return (
-//     <Layout 
-//       header={
-//         <Header>
-//           <Searchbar onSearch={inputValue => handleSearch(inputValue)}/>
-//         </Header> }
-//       menu={<Menu />}
-//       content={          
-//         loading ? 
-//         (<LoadingIcon />
-//         ) :(
-//         <Hotels hotels={hotels}/>)}
-//       footer={<Footer/>}
-//     />
-//   );
-// };
-
-// export default App;
